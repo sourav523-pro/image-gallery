4d30a9105235e58155951f33aca7079eb6c7538d
sourav <samantasourav184@gamil.com>
2022-03-01 00:27:11 +0530

    download type added

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import SearchButton from "../tailwindcomp/SearchButton"
 import ProcessingButton from "../tailwindcomp/ProcessingButton"
 import ProcessingOptions from "../tailwindcomp/ProcessingOptions"
+import DownloadOptions from "../tailwindcomp/DownloadOptions"
 import RelatedVideos from './RelatedVideos'
 const Youtube = () => {
     const [search, setSearch] = useState(
@@ -13,7 +14,8 @@ const Youtube = () => {
             failedMsg: '',
         }
     )
-    const getInfo = () => {
+    const getInfo = (e) => {
+        e.preventDefault()
         let searchUrl = search.url
         // let re = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/watch\?v=([^&]+)/m;
         if (!searchUrl || matchYoutubeUrl(searchUrl) === false) {
@@ -38,32 +40,55 @@ const Youtube = () => {
         }
         return false;
     }
+    // const downloadVideo = (mimeType, quality) => {
+
+    // }
     const handleUrlChange = (e) => {
         setSearch({ ...search, url: e.target.value })
     }
+    // let videoFormats = search.infoData.formats || []
+    // let videoId = search.infoData.videoDetails.videoId || ''
+    let videoId = ''
     return (
         <>
             <div className="mt-5 mx-5">
                 <div className="flex justify-center">
                     {/* <div className="mb-3 xl:w-96"> */}
                     <div className="mb-3">
-                        <div className="group input-group relative flex justify-center items-stretch w-full mb-4 rounded">
+                        <form onSubmit={getInfo} className="group input-group relative flex justify-center items-stretch w-full mb-4 rounded">
                             <input type="text"
                                 className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-xl shadow m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none hover:shadow-lg"
                                 placeholder="Youtube URL"
                                 value={search.url}
                                 onChange={handleUrlChange}
                             />
-                            {search.flag ? <ProcessingButton /> : <SearchButton handleClick={getInfo} />}
-                        </div>
+                            {search.flag ? <ProcessingButton /> : <SearchButton buttonType="submit" />}
+                        </form>
                     </div>
                 </div>
                 <div className="mt-5 mx-5">
                     <h1 className="text-gray-700 text-xl">{search.flag === false && search.dataFetched ? 'Available Formates' : 'Available Formates'}</h1>
                     {
-                        [1, 2, 3, 4].map((val, index) => (
+                        search.flag ? [1, 2, 3, 4].map((val, index) => (
                             <ProcessingOptions key={index} />
                         ))
+                            :
+                            search.dataFetched ?
+                                search.infoData.formats.map((val, index) => {
+                                    let [mediaType, options] = val.mimeType.split('; ')
+                                    let [type, extension] = mediaType.split('/')
+                                    return <DownloadOptions
+                                        key={index}
+                                        quality={val.quality}
+                                        qualityLabel={val.qualityLabel}
+                                        type={type}
+                                        mediaType={mediaType}
+                                        extension={extension}
+                                        videoId={videoId}
+                                    />
+                                })
+                                :
+                                ''
                     }
                 </div>
 
--- src/tailwindcomp/SearchButton.jsx
+++ src/tailwindcomp/SearchButton.jsx
@@ -1,7 +1,12 @@
 import { SearchIcon } from '@heroicons/react/outline'
-const SearchButton = ({ handleClick }) => {
+const SearchButton = (props) => {
+    const defaultClick = () => {
+        //empty function
+    }
+    let handleClick = props.handleClick || defaultClick
+    let buttonType = props.buttonType || 'submit'
     return (
-        <button type="button" onClick={handleClick} className="px-3 py-1.5 flex items-center justify-center mx-4 rounded-md text-white bg-indigo-500 hover:bg-indigo-400">
+        <button type={buttonType} onClick={handleClick} className="px-3 py-1.5 flex items-center justify-center mx-4 rounded-md text-white bg-indigo-500 hover:bg-indigo-400">
             <SearchIcon className='h-5 w-5 text-white font-bold' />
         </button>
     )
